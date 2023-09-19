b19ccbfc33e0f99ef69d9d13f84ec82a743d1f63
David <jd_015@hotmail.com>
2023-09-19 18:44:42 -0500

    add web-server and update requirements.txt

---
+++ web-server/main.py
@@ -0,0 +1,26 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3]
+
+@app.get('/contact')
+def get_list():
+    return {'name': 'Platzi'}
+
+@app.get('/html', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Hola soy una página</h1>
+        <p>Soy un párrafo</p>
+    """
+
+def run():
+    store.get_categories()
+
+if __name__ == '__main__':
+    run()
